9b2b6698f30076d0d5721cae628023e280a84269
schismatiic <leonardojofre2003@gmail.com>
2026-06-28 16:18:46 -0400

    Type y description para variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,10 @@
-variable "region" { default = "us-east-1" }
-variable "project" { default = "hito1-leo-emi" }
+variable "region" { 
+    type = string 
+    default = "us-east-1"
+    description = "Región de AWS utilizada para el proyecto"
+}
+variable "project" { 
+    type = string 
+    default = "hito1-leo-emi"
+    description = "Nombre del proyecto"
+}
