6639eddb8a679e83b636631280e7f13608535a7c
Nurodayo <cianuroicecream@gmail.com>
2026-07-09 19:26:36 -0400

    funca la api

--- main.tf
+++ main.tf
@@ -465,3 +465,25 @@ resource "aws_lambda_function" "reservas" {
     }
   }
 }
+
+resource "aws_iam_policy" "lambda_logs" {
+  name = "LambdaCloudWatchLogs"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logs.arn
+}
